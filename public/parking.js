fbc3130d0c7a56677daa3c7cbb3d16ddf7146f20
PuckettAB <79866000+PuckettAB@users.noreply.github.com>
2025-11-15 15:16:07 -0500

    Add back service worker

--- public/parking.js
+++ public/parking.js
@@ -1,6 +1,6 @@
-/* if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js");
-} */
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   function parseParams() {
